938c82bc3b78067b4d914c9ba2a5fd27678a1a74
John Hardy <jh@lagado.com>
2026-01-28 11:26:07 +1100

    Update default targets to 'serial' and add serial echo programs for TEC-1G MON-1 and MON-3

--- tec1g-mon3/src/main.asm
+++
@@ -1,9 +0,0 @@
-; TEC-1G MON-3 starter program
-; Target: TEC-1G (MON-3), user program base 0x4000
-;
-; This is a placeholder program that sits in a tight loop.
-; Replace with your own code as you explore the platform.
-
-        .ORG    0x4000
-
-START:  JP      START
